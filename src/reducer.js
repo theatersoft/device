d6591fb299d70f3a7eab38cfebabbb69edd76ae6
Rob Huehn <rhuehn@gmail.com>
2017-01-12 19:56:26 -0800

    set state for single device

--- src/reducer.js
+++ src/reducer.js
@@ -3,28 +3,43 @@ import {SET_TIME, SET_STATE} from './actions'
 
 const devices = (state = [], action) => {
     if (action.type === SET_STATE) {
-        const {name, state: {devices}} = action
-        return [
-            ...(state.filter(d => !d.id.startsWith(`${name}.`))),
-            ...(devices.map(d => ({...d, id: `${name}.${d.id}`})))
-        ]
+        const {name, state: {devices, device}} = action
+        if (devices)
+            return [
+                ...(state.filter(({id}) => !id.startsWith(`${name}.`))),
+                ...(devices.map(d => ({...d, id: `${name}.${d.id}`})))
+            ]
+        else
+            return [
+                ...(state.filter(({id}) => id !== name)),
+                {...device, id: name}
+            ]
     }
     return state
 }
 
 const values = (state = {}, action) => {
     if (action.type === SET_STATE) {
-        const {name, state: {values}} = action
-        return {
-            ...Object.entries(state).reduce((o, [k, v]) => {
-                if (!k.startsWith(`${name}.`)) o[k] = v
-                return o
-            }, {}),
-            ...Object.entries(values).reduce((o, [k, v]) => {
-                o[`${name}.${k}`] = v
-                return o
-            }, {})
-        }
+        const {name, state: {values, value}} = action
+        if (values)
+            return {
+                ...Object.entries(state).reduce((o, [k, v]) => {
+                    if (!k.startsWith(`${name}.`)) o[k] = v
+                    return o
+                }, {}),
+                ...Object.entries(values).reduce((o, [k, v]) => {
+                    o[`${name}.${k}`] = v
+                    return o
+                }, {})
+            }
+        else
+            return {
+                ...Object.entries(state).reduce((o, [k, v]) => {
+                    if (!k.startsWith(`${name}.`)) o[k] = v
+                    return o
+                }, {}),
+                [name]: value
+            }
     }
     return state
 }
