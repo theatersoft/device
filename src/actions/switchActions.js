d7f4d26598a1b98ddb0755f831d5a5f16365b637
Rob Huehn <rhuehn@gmail.com>
2017-07-05 10:54:22 -0700

    add toggle to switch actions

--- src/actions/switchActions.js
+++ src/actions/switchActions.js
@@ -2,4 +2,5 @@ export const
     ON = 'ON',
     OFF = 'OFF',
     on = id => ({type: ON, id}),
-    off = id => ({type: OFF, id})
+    off = id => ({type: OFF, id}),
+    toggle = (value, id) => ({type: value ? OFF : ON, id})
