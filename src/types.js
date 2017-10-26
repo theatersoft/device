4b98edd1a013ae8b5f20dadce9a7cf396e0180fd
Rob Huehn <rhuehn@gmail.com>
2017-10-25 18:02:25 -0700

    add button binary sensor

--- src/types.js
+++ src/types.js
@@ -7,6 +7,7 @@ export const
     },
     Type = {
         BinarySensor: 'BinarySensor',
+        Button: 'Button',
         Chime: 'Chime',
         Dimmer: 'Dimmer',
         DuskSensor: 'DuskSensor',
@@ -21,6 +22,7 @@ export const
         Switch: 'Switch'
     },
     interfaceOfType = type => ({
+        [Type.Button]: Interface.SENSOR_BINARY,
         [Type.Chime]: Interface.SWITCH_BINARY,
         [Type.Dimmer]: Interface.SWITCH_MULTILEVEL,
         [Type.DuskSensor]: Interface.SENSOR_BINARY,
