35a326ea45a5e40fa41df9b72963dc5a29d2e0c7
Rob Huehn <rhuehn@gmail.com>
2017-10-25 17:59:54 -0700

    tidy

--- src/types.js
+++ src/types.js
@@ -6,30 +6,30 @@ export const
         SENSOR_MULTILEVEL: 'SensorMultilevel'
     },
     Type = {
-        Switch: 'Switch',
-        LightSwitch: 'LightSwitch',
-        PowerSwitch: 'PowerSwitch',
-        Dimmer: 'Dimmer',
-        Receptacle: 'Receptacle',
         BinarySensor: 'BinarySensor',
-        OpenSensor: 'OpenSensor',
-        MotionSensor: 'MotionSensor',
-        DuskSensor: 'DuskSensor',
-        Projector: 'Projector',
-        Siren: 'Siren',
         Chime: 'Chime',
-        Hvac: 'Hvac'
+        Dimmer: 'Dimmer',
+        DuskSensor: 'DuskSensor',
+        Hvac: 'Hvac',
+        LightSwitch: 'LightSwitch',
+        MotionSensor: 'MotionSensor',
+        OpenSensor: 'OpenSensor',
+        PowerSwitch: 'PowerSwitch',
+        Projector: 'Projector',
+        Receptacle: 'Receptacle',
+        Siren: 'Siren',
+        Switch: 'Switch'
     },
     interfaceOfType = type => ({
-        [Type.Switch]: Interface.SWITCH_BINARY,
-        [Type.LightSwitch]: Interface.SWITCH_BINARY,
-        [Type.PowerSwitch]: Interface.SWITCH_BINARY,
-        [Type.Receptacle]: Interface.SWITCH_BINARY,
+        [Type.Chime]: Interface.SWITCH_BINARY,
         [Type.Dimmer]: Interface.SWITCH_MULTILEVEL,
-        [Type.OpenSensor]: Interface.SENSOR_BINARY,
-        [Type.MotionSensor]: Interface.SENSOR_BINARY,
         [Type.DuskSensor]: Interface.SENSOR_BINARY,
+        [Type.LightSwitch]: Interface.SWITCH_BINARY,
+        [Type.MotionSensor]: Interface.SENSOR_BINARY,
+        [Type.OpenSensor]: Interface.SENSOR_BINARY,
+        [Type.PowerSwitch]: Interface.SWITCH_BINARY,
         [Type.Projector]: Interface.SWITCH_BINARY,
+        [Type.Receptacle]: Interface.SWITCH_BINARY,
         [Type.Siren]: Interface.SWITCH_BINARY,
-        [Type.Chime]: Interface.SWITCH_BINARY
+        [Type.Switch]: Interface.SWITCH_BINARY
     }[type])
